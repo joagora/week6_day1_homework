38690375acf0e23eb6f6c8806164ce4c2886dee9
Joanna Gora <joanna.gora@protonmail.com>
2018-07-30 16:41:22 +0100

    created subtractValues method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,7 +2,12 @@ public class Calculator {
 
 
     public int addValues(int value1, int value2){
-        int sum = value1 + value2;
-        return sum;
+        int result = value1 + value2;
+        return result;
+    }
+
+    public int subtractValues(int value1, int value2){
+        int result = value1 - value2;
+        return result;
     }
 }
