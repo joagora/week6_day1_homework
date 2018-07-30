e2f37c09bfee58c78f8f007dee0780b639ac6596
Joanna Gora <joanna.gora@protonmail.com>
2018-07-30 16:38:55 +0100

    created addValues method

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+
+
+    public int addValues(int value1, int value2){
+        int sum = value1 + value2;
+        return sum;
+    }
+}
---
+++ src/main/java/Runner.java
@@ -0,0 +1,6 @@
+public class Runner {
+    public static void main(String[] args){
+        Calculator newCalculator = new Calculator();
+        System.out.println(newCalculator.addValues(1, 2));
+    }
+}
